f1ca6b9013f69dadb130ec50d993998cc36b73f8
Esau-Morales <lextospawn@gmail.com>
2025-12-24 16:39:18 -0600

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+#Haz un programa en python que pida por teclado al usuario su nombre, su edad y su altura en metros.
+
+nombre = input("Cual es tu nombre?")
+Edad = int(input("Dame tu edad:"))
+Altura = int(input("Dame tu altura en metros: "))
+
+print ("El usuario ",nombre, "de ", Edad, "y mide ", Altura, "metros.")
