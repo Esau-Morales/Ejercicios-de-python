32f4ff75d98201cb7bd7cfd07d90d76b515cb381
Esau-Morales <lextospawn@gmail.com>
2025-12-24 16:44:38 -0600

    Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,5 +4,5 @@ nombre = input("Cual es tu nombre?")
 Edad = int(input("Dame tu edad:"))
 Altura = float(input("Dame tu altura en metros: "))
 
-
 print ("El usuario ",nombre, "de ", Edad, "y mide ", Altura, "metros.")
+
